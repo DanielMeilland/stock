1eb943cc66f481678229cbc9b096b5be7abbf1f3
jeffrey Mostroso <n3tsky@me.com>
2016-01-19 14:03:04 +0100

    push 14:03

---
+++ application/modules/authentification/config/routes.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MoJe
+ * Date: 19.01.2016
+ * Time: 11:50
+ */
+
+$route['authentification/login'] = 'authentification/login';
+$route['authentification/logout'] = 'authentification/logout';
---
+++ application/modules/users/config/routes.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MoJe
+ * Date: 19.01.2016
+ * Time: 11:52
+ */
